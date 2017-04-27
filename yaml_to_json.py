6a25a8089cb9ec7c802bb50160d40a06de403164
rklimenko <rklimenko@itkey.com>
2017-04-27 15:04:26 +0300

    added yaml_to_json

---
+++ yaml_to_json.py
@@ -0,0 +1,13 @@
+import yaml, json, sys, argparse
+
+parser = argparse.ArgumentParser(description="Yaml to JSON decoder")
+parser.add_argument("--source", type=str, help="Yaml file", required=True)
+parser.add_argument("--dest", type=str, help="JSON file", required=True)
+args = parser.parse_args()
+
+with open(args.source, 'r') as stream:
+    try:
+        with open(args.dest, 'w') as outfile:
+            json.dump(yaml.load(stream), outfile)
+    except yaml.YAMLError as exc:
+        print(exc)
